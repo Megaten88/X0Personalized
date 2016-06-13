14f307266a67f28874a81bb22474e545d3ab9f3a
Mayra Salazar <marialejandra200918@gmail.com>
2016-06-12 19:24:53 -0600

    First X0

--- Proyecto_MayraSalazar/src/proyecto_mayrasalazar/Main.java
+++ Proyecto_MayraSalazar/src/proyecto_mayrasalazar/Main.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package proyecto_mayrasalazar;
-
+import java.util.Scanner;
 /**
  *
  * @author Mayra Salazar
@@ -15,8 +15,17 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.print("Hello Mayra");
+        X0_PVP playPvP = new X0_PVP(); 
+        Scanner read = new Scanner(System.in);
+        int x;
+        int y;
+        do{
+            playPvP.leerMatriz();
+            System.out.println("Ingrese movimiento filas:");
+            x = read.nextInt();
+            System.out.println("Ingrese movimiento columnas:");
+            y = read.nextInt();
+            playPvP.mover(x, y);
+        }while(playPvP.gameEnd());
     }
-    
 }
